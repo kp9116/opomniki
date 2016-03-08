85f4eac0442fb54542691e25a5a6d974fe0296fd
Klemen Papez <kp9116@student.uni-lj.si>
2016-03-08 14:55:55 +0000

    implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,11 +1,21 @@
 window.addEventListener('load', function() {
 	//stran nalozena
 		
+	
+		
+	var izvediPrijavo = function (event){
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility ="hidden";
+	}
+					
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
